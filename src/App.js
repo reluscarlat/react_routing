9eea09239763ccb227dbfe675b80271c147b7316
relu <relu.scarlat@gmail.com>
2019-03-27 04:34:07 +0200

    Commit 1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+
+const Route = require("react-router-dom").Route;
+const Router = require("react-router-dom").BrowserRouter;
+const Link = require("react-router-dom").Link;
+const NavLink = require("react-router-dom").NavLink;
+const Redirect = require("react-router-dom").Redirect;
+const Prompt = require("react-router-dom").Prompt;
+
+
+const User = ({matchContent})=> {
+  return(<h1>Welcome, {matchContent.params.username}!</h1>)
+}
+
+const actStyle = {
+  color : "green",
+  backgroundColor: "red"
+}
 
 class App extends Component {
+
+  state = {
+    loggedIn: false
+  }
+
+  loginHandle = ()=> {
+    this.setState(prevState=>{
+      return ({loggedIn : !prevState.loggedIn})
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          
+          <ul>
+            <li>
+              <NavLink activeStyle={actStyle} exact to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={actStyle} exact to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={actStyle} exact to="/user/John">Hello John</NavLink>
+            </li><li>
+              <NavLink activeStyle={actStyle} exact to="/user/Bob">Hello Bob</NavLink>
+            </li>
+          </ul>
+
+          <input type="button" value={!this.state.loggedIn ? "Log in" : "Log out" } onClick={this.loginHandle.bind(this)}></input>
+
+          <Route path="/" exact strict render={()=>{
+            return (<h1>Welcome Home</h1>)
+          }}/>       
+
+          <Route path="/about" exact strict render={()=>{
+            return (<h1>About</h1>)
+          }}/>  
+
+          <Route path="/user/:username" exact strict render={({match})=>{ 
+            return(this.state.loggedIn ? (<User matchContent={match} />) : (<Redirect to="/" />))
+          }}/>  
+
+          <Prompt 
+            when={!this.state.loggedIn}
+            // message={(location)=>{
+            //   return location.pathname.startsWith("/user") ? "Are you sure?" : true
+            // }}
+            message={"Are you sure???"}
+          />
+
+        </div>
+      </Router>
     );
   }
 }
